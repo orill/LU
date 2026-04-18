20c8c0ddde08037e0983eb0b9ad7a1d158b85abe
ORILL <39681553+orill@users.noreply.github.com>
2026-04-18 15:42:14 +0200

    Update LU.c

--- LU.c
+++ LU.c
@@ -1,6 +1,6 @@
 // **********************************************************************
 // Ultra small LU in C - https://en.wikipedia.org/wiki/LU_decomposition
-// Factorize square matrix MAT into LU = MAT, solve x in LUx = b
+// Factorize square matrix MAT into LU = MAT, solve x in LU.x = b
 // 2006-2026, Franck CHANTELOUP, Institut Laue Langevin, Grenoble, France
 // **********************************************************************
 
